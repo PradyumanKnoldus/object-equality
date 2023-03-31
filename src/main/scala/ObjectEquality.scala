cee5ab76d168c46c9b117eae5495768b68f58b97
PradyumanKnoldus <pradyuman.singh@knoldus.com>
2023-03-31 23:22:20 +0530

    Added Object Equality Assignment

--- src/main/scala/ObjectEquality.scala
+++ src/main/scala/ObjectEquality.scala
@@ -1,31 +1,46 @@
-
 package com.knoldus
+
+import scala.util.{Failure, Success, Try}
 
 class ObjectEquality {
 
   // equals method - checks if two objects are equal
   def equalsMethod[T](firstObject: T, secondObject: T): Boolean = {
-    firstObject.equals(secondObject)
+    Try(firstObject.equals(secondObject)) match {
+      case Success(value) => value
+      case Failure(_) => false
+    }
   }
 
   // == operator - checks if two objects are equal
   def doubleEqualsOperator[T](firstObject: T, secondObject: T): Boolean = {
-    firstObject == secondObject
+    Try(firstObject == secondObject) match {
+      case Success(value) => value
+      case Failure(_) => false
+    }
   }
 
   // != operator - checks if two objects are not equal
   def notEqualsOperator[T](firstObject: T, secondObject: T): Boolean = {
-    firstObject != secondObject
+    Try(firstObject != secondObject) match {
+      case Success(value) => value
+      case Failure(_) => false
+    }
   }
 
   // ne method - checks if two references refer to different objects
   def neMethod[T <: AnyRef](firstObject: T, secondObject: T): Boolean = {
-    firstObject.ne(secondObject)
+    Try(firstObject.ne(secondObject)) match {
+      case Success(value) => value
+      case Failure(_) => false
+    }
   }
 
   // eq method - checks if two references refer to the same object
   def eqMethod[T <: AnyRef](firstObject: T, secondObject: T): Boolean = {
-    firstObject.eq(secondObject)
+    Try(firstObject.eq(secondObject)) match {
+      case Success(value) => value
+      case Failure(_) => false
+    }
   }
-
 }
